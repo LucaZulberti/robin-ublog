ef7cb00e7c11c4434be928cfa49e7e8e61f3ff12
Luca Zulberti <l.zulberti@studenti.unipi.it>
2020-05-06 18:34:52 +0200

    robin_server.c: add usage and parse arguments
    Signed-off-by: Luca Zulberti <l.zulberti@studenti.unipi.it>

--- robin/src/robin_server.c
+++ robin/src/robin_server.c
@@ -10,12 +10,24 @@
  * Luca Zulberti <l.zulberti@studenti.unipi.it>
  */
 #include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
 
 #include "robin_server.h"
 #include "robin.h"
+
+/*
+ * Usage
+ */
+void usage(void)
+{
+    puts("usage: robin_server <host> <port>");
+    puts("\thost: hostname where the server is executed");
+    puts("\tport: port on which the server will listen for incoming "
+         "connections");
+}
 
 /*
  * TCP parameters
@@ -30,7 +42,31 @@ static robin_thread_t *robin_thread_pool;
 
 int main(int argc, char **argv)
 {
-    /* Allocate memory for thread structures */
+    size_t host_len;
+
+    /* parse arguments */
+    if (argc != 3) {
+        robin_log_err("invalid number of arguments.");
+        usage();
+        exit(EXIT_FAILURE);
+    }
+
+    /* save host string */
+    host_len = strlen(argv[1]);
+    host = malloc((host_len + 1) * sizeof(char));
+    if (!host) {
+        robin_log_err("%s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    memcpy(host, argv[1], host_len);
+
+    /* parse port */
+    port = atoi(argv[2]);
+
+    robin_log_info("Starting Robin server... (host: %s, port: %d)",
+                   host, port);
+
+    /* allocate memory for thread structures */
     robin_thread_pool = malloc(ROBIN_SERVER_THREAD_NUMBER
                                * sizeof(robin_thread_t));
     if (!robin_thread_pool) {
